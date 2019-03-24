680100282a134d8f5190f1614433500cf27a4cb9
Niyati Bafna <niyatibafna13@gmail.com>
2019-03-24 16:58:47 +0530

    Attempt to remove RT

--- scraper.py
+++ scraper.py
@@ -20,20 +20,23 @@ auth.set_access_token("1612765604-VHcRtBok33g2phCJ7LqzXfaW5ETWXbDyQ9ZtOsV", "8Nt
 
 api = tweepy.API(auth, wait_on_rate_limit= True)
 
-file = open("dalit.txt", "w")
+file = open("dalit4withoutRT.txt", "w")
 count = 0 
 
 f = open("D_SearchTerms.txt", "r")
 
 try : 
+	previous=""
 	for line in f:
-	    for tweet in tweepy.Cursor(api.search,tweet_mode='extended', q=line, rpp=100).items(30000):
-	        if ("India" in tweet._json["user"]["location"]):   
-	      
-	            print tweet.full_text.encode('utf-8')
-	            file.write(tweet.full_text.encode('utf-8'))
-	            count += 1  
-	    print count
+		print(line)
+		for tweet in tweepy.Cursor(api.search,tweet_mode='extended', q=line, rpp=100).items(30000):
+			if ("India" in tweet._json["user"]["location"]):   
+				print tweet.full_text.encode('utf-8')
+				if(tweet!=previous):
+					file.write(tweet.full_text.encode('utf-8'))
+					count += 1  
+				previous = tweet
+	print count
 
 except tweepy.TweepError:  
 
