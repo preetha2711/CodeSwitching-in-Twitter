fd7926e54892c221a029e57ba2781433e012c031
preetha2711 <datta.preetha@gmail.com>
2019-04-03 17:22:39 +0530

    made changes to clean_RT's

--- clean_RT.py
+++ clean_RT.py
@@ -7,14 +7,27 @@ non_RT_arr = []
 for line in file: 
     f.write(line)
     if line == "\n":
-        f.write("!!!!!!!!!EOT!!!!!!!!!")
+        f.write("!!!!!!!!!EOT!!!!!!!!!" + "\n")
     if "RT @" in line:
-        f.write("\n"+"!!!!!!!!!EOT!!!!!!!!!!!")
-    f.write(line + "\n"+"!!!!!!!!!EOT!!!!!!!!!!!")
+        f.write("\n"+"!!!!!!!!!EOT!!!!!!!!!!!" + "\n")
+    f.write(line + "\n"+"!!!!!!!!!EOT!!!!!!!!!!!" +"\n")
 
 
 
+f_read = open("Seg_Collated.txt", "r")
+f_write = open("Seg_Collated_NonRT.txt", "w")
+
+for line in f_read:
+    if "RT @" in line:
+        continue
+    elif line == "\n":
+        continue
+    elif (("!!!!!!!!!EOT!!!!!!!!!" in line) or ("!!!!!!!!!EOT!!!!!!!!!!!" in line)):
+        continue
+    else :
+        f_write.write(line)
 
     
+
 
 
