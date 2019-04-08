75fd71ebab842607ad133d7d5c091393b42cb45b
preetha2711 <datta.preetha@gmail.com>
2019-04-09 02:52:52 +0530

    results added as comment

--- word_count.py
+++ word_count.py
@@ -1,4 +1,4 @@
-file = open("tweets_clean.txt","r")
+file = open("fem_tweets_tagged.txt","r")
 en = 0
 hi = 0
 hi_dev = 0
@@ -9,12 +9,13 @@ for line in file:
         maxchar = max(line)
         # print words
         for word in words:
-            if "/EN" in word:
+            if (("/EN" in word) and ("thisisanewtweet" not in word) and ("fullstop" not in word) and ("questionmark" not in word) and ("exclamationmark" not in word) ):
                 en +=1
             elif "/HI" in word:
                 hi +=1
             elif u'\u0900' <= maxchar <= u'\u097f':
                 hi_dev +=1
+            
 
             
             
@@ -23,3 +24,10 @@ for line in file:
 print en
 print hi
 print hi_dev
+
+'''
+Results : 
+280754
+57928
+205867
+'''
