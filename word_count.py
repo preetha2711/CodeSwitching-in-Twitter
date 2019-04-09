3ed1aaf3d8ae287785b60346bc64b6595d0263e6
Niyati Bafna <niyatibafna13@gmail.com>
2019-04-09 07:17:51 +0530

    Eliminate counting sentences with no words

--- word_count.py
+++ word_count.py
@@ -11,13 +11,18 @@ for line in file:
         line = line.decode("utf-8")
         maxchar = max(line)
         # print words
+
         for word in words:
+            print(word)
             if (("/EN" in word) and ("thisisanewtweet" not in word) and ("fullstop" not in word) and ("questionmark" not in word) and ("exclamationmark" not in word) ):
                 en +=1
+                print("EN \n \n \n")
             elif "/HI" in word:
                 hi +=1
+                print("HI \n \n \n")
             elif u'\u0900' <= maxchar <= u'\u097f':
                 hi_dev +=1
+                print("HIDEV \n \n \n")
             
         
             
@@ -33,7 +38,7 @@ for tweet in tweets:
         hidev_per_sent = 0
         sentence = sentence.split(" ")
         for word in sentence:
-            if word == "":
+            if word == "" or word == " ":
                 pass
             else:
                 word = word.decode("utf-8")
@@ -46,16 +51,27 @@ for tweet in tweets:
             elif u'\u0900' <= maxchar <= u'\u097f':
                     hidev_per_sent +=1
 
-        if hidev_per_sent == max(hidev_per_sent,hi_per_sent,eng_per_sent):
-            sent_hin +=1
-        elif hi_per_sent == max(hidev_per_sent,hi_per_sent,eng_per_sent):
-            sent_hin +=1
-        else:
-            sent_eng +=1
+        print(sentence) 
+        if(hi_per_sent + hi_per_sent + eng_per_sent != 0):
+            if(hi_per_sent + hi_per_sent > eng_per_sent):
+                sent_hin +=1
+                #print("HI \n \n \n")
+            else:
+                sent_eng +=1
+                #print("EN \n \n \n")
 
-        
 
-        
+        # if hidev_per_sent == max(hidev_per_sent,hi_per_sent,eng_per_sent):
+        #     sent_hin +=1
+        #     print("HI \n \n \n")
+        # elif hi_per_sent == max(hidev_per_sent,hi_per_sent,eng_per_sent):
+        #     sent_hin +=1
+        #     print("HI \n \n \n")
+        # else:
+        #     sent_eng +=1
+        #     print("EN \n \n \n")
+
+    
 
 print en
 print hi
@@ -72,4 +88,15 @@ Results :
 
 27684
 15085
+'''
+
+'''
+Alternate results with all zero sentences removed:
+280754
+57928
+205867
+
+
+32254
+4352
 '''
