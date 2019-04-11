5959936957e11796b6d01d85ed2ab15bf5850269
preetha2711 <datta.preetha@gmail.com>
2019-04-12 03:03:40 +0530

    added analyses + dalit tweet sepearator

--- dalit_sep_tweets.py
+++ dalit_sep_tweets.py
@@ -1,22 +1,41 @@
-file = open("Dalit_Collated_Cleaned.txt", "r")
-f2 = open("Dalit_col_fin.txt", "w")
+# file = open("Dalit_Collated_Cleaned.txt", "r")
+# f2 = open("Dalit_col_fin.txt", "w")
 
+# for line in file:
+# 	line = line.decode("utf-8")
+# 	words = line.split(" ")
+# 	for word in words:
+# 		if(word == " " or word == ""):
+# 			continue
+# 		count_sent = 0
+# 		#word = word.decode('utf-8')
+# 		f2.write(word.encode("utf-8").strip())
+
+# 		if word == "sentenceender":
+# 			f2.write("\n")
+# 			count_sent += 1
+# 		else:
+# 			f2.write(" ")
+
+
+# file.close()
+# f2.close()
+
+file = open("Dalit_tagged.txt", "r")
+f3 = open("final_dal.txt", "w")
+count_sent = 0
+count_word = 0
+tweet = "thisisanewtweet "
 for line in file:
-	line = line.decode("utf-8")
-	words = line.split(" ")
-	for word in words:
-		if(word == " " or word == ""):
-			continue
-		count_sent = 0
-		#word = word.decode('utf-8')
-		f2.write(word.encode("utf-8").strip())
+	if "##" not in line:
+		words = line.split(" ")
+		for word in words: 
+			f3.write(word + " ")
+			count_word +=1
+			if ("sentenceender" in word):
+				count_sent +=1	
+			if count_sent == 3:
+				f3.write("\n"+"thisisanewtweet" + "\n")
+				count_sent = 0
 
-		if word == "sentenceender":
-			f2.write("\n")
-			count_sent += 1
-		else:
-			f2.write(" ")
-
-
-file.close()
-f2.close()
+				
